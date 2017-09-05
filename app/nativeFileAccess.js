ee86b936f329331be13e4fe4bbc2277d2474ea1f
Mark McCoid <markmccoid@gmail.com>
2017-09-05 14:45:39 -0400

    updates

--- app/nativeFileAccess.js
+++ app/nativeFileAccess.js
@@ -7,7 +7,7 @@ const { remote } = require('electron');
 const prodBackup = require('./upgrade/productionBackup');
 const copyUpgradeFiles = require('./upgrade/copyUpgradeFiles');
 const { renameAsarToHold, renameHoldToAsar } = require('./copyIncludeDir');
-const { mergeQVVars } = require('./upgrade/mergeFiles');
+const { mergeQVVars, writeXMLData } = require('./upgrade/mergeFiles');
 console.log(mergeQVVars);
 const SETTINGS_FILE = process.env.NODE_ENV === 'development' ?
 											path.join(remote.app.getAppPath(),'AnalytixInstallerSettings.json') :
@@ -70,6 +70,8 @@ const installAnalytix = productionFolder => {
 		.catch((err) => ({status: 'error', msg: stringifyError(err)})) //--if error return 'error' status
 }
 
+//--Backs up production folder to the designated backup folder.
+//--Not a straight copy, doesn't backup QVD files, etc.
 const productionBackup = (productionFolder, backupFolder) => {
 	let upgradeFolder = getLocalPath('');
 	console.log(upgradeFolder);
@@ -78,21 +80,42 @@ const productionBackup = (productionFolder, backupFolder) => {
 		.catch((err) => ({status: 'error', msg: stringifyError(err)}));
 }
 
-const upgradeAnalytixFiles = (productionFolder, backupFolder) => {
+//--Copies the upgrade files into the production folder.  backupFolder is not currently used.
+const upgradeAnalytixFiles = (productionFolder, backupFolder = '') => {
 	let upgradeFolder = getLocalPath('');
 	return copyUpgradeFiles(productionFolder, upgradeFolder, backupFolder)
 		.then(() => ({status: 'finished', msg: 'Analytix Copy Files Complete'}))
 		.catch((err) => ({status: 'error', msg: stringifyError(err)}));
 };
 
+//--Merges the qvVariables.json from upgrade with the production version then writes out new XML filess to spreadsheet directory.
+//--Merges the qvGroups.json from upgrade with the production version then writes out new XML filess to spreadsheet directory.
 const mergeFiles = (productionFolder) => {
 	let upgradeFolder = getLocalPath('');
 	let siteQVVarsFile = path.join(productionFolder, 'include/VariableEditor/data/SITE_qvVariables.json');
 	let upgQVVarsFile = path.join(productionFolder, 'include/VariableEditor/data/qvVariables.json');
 
 	return mergeQVVars(siteQVVarsFile, upgQVVarsFile, productionFolder)
+		.then(exportVariableXMLFiles(productionFolder))
 		.then(response => ({status: 'finished', msg: response}))
 		.catch(err => ({status: 'error', msg: stringifyError(err)}));
+}
+
+const exportVariableXMLFiles = (productionFolder) => {
+		let spreadsheetFolder = path.join(productionFolder, '/Include/Spreadsheets');
+		let qvVarsFile = path.join(productionFolder, '/Include/VariableEditor/data/qvVariables.json');
+		let qvVarsObj = JSON.parse(fs.readFileSync(qvVarsFile));
+		let applicationList = _.uniq(qvVarsObj.map(varObj => varObj.application));
+		//--will return a promise
+		return writeXMLData(applicationList, qvVarsObj, spreadsheetFolder, 'variable');
+}
+const exportGroupXMLFiles = (productionFolder) => {
+		let spreadsheetFolder = path.join(productionFolder, '/Include/Spreadsheets');
+		let qvVarsFile = path.join(productionFolder, '/Include/GroupEditor/data/qvgroups.json');
+		let qvVarsObj = JSON.parse(fs.readFileSync(qvVarsFile));
+		let applicationList = _.uniq(qvVarsObj.map(varObj => varObj.application));
+		//--will return a promise
+		return writeXMLData(applicationList, qvVarsObj, spreadsheetFolder, 'group');
 }
 
 module.exports = {
--- app/upgrade/mergeFiles.js
+++ app/upgrade/mergeFiles.js
@@ -9,6 +9,7 @@
 //-------------------------------------------------------
 const fs = require('fs-extra');
 const path = require('path');
+const X2JS = require('x2js');
 
 //Create a function that accepts a filename and returns a promise
 //that will resolve with the file contents when done being read
@@ -23,17 +24,40 @@ const fileReadPromise = file => {
 	});
  };
 
- const fileWritePromise = file => {
-  	return new Promise((resolve, reject) => {
- 	  fs.readFile(file, 'utf8', (err, data) => {
- 	    if (err) {
- 	      reject(Error(err));
- 	    }
- 	    resolve(data);
- 	  });
- 	});
-  };
+//==========================================
+//--writeXMLData function
+//==========================================
+//Takes the appName and writes out an XML file of the groups data to the Spreadsheets directory
+//returns the applicationGroups data
+//-Params:
+//-- appNameArray - Array of applications in the qvObj.  Will be used to loop so we can write a file for each application
+//-- qvObj - Object containing either variables or groups
+//-- spreadsheet_DIR - full path to the the spreadsheet directory
+//-- XMLContainer - either 'variable' or 'group' this is the XML container around each xml item (variable or group)
+const writeXMLData = (appNameArray, qvObj, spreadsheet_DIR, XMLContainer = 'variable') => {
+//--We need 1. appNameArray, PROD_DIR, applicationVars
 
+	writeXMLPromiseArray = appNameArray.map(appName => {
+		let appNameSansSpaces = appName.replace(/\s+/g, '');
+		//determine filename to write out to based on if this is for Groups or Variables.
+		let appFileName = `${appName}${XMLContainer === 'group' ? 'groups' : ''}.xml`
+		const x2js = new X2JS();
+		let applicationVars = qvObj.filter(qvVar => qvVar.application.toLowerCase() === appName.toLowerCase());
+		let xmlString = x2js.js2xml({[XMLContainer]: applicationVars});
+		//Enclose xml created with the appName, otherwise Qlik won't recognize properly
+		applicationVars = `<${appNameSansSpaces}>${xmlString}</${appNameSansSpaces}>`;
+		//write the groups array back to the server disk navigating to the include directory
+		let xmlFilePathName = path.join(spreadsheet_DIR, appFileName);
+		//Since using fs-extra, writeFile has been promisified
+		return fs.writeFile(xmlFilePathName, applicationVars);
+	});
+
+	return Promise.all(writeXMLPromiseArray);
+
+}
+
+
+//==========================================
 //--Entry point for merging the QVVars files
 const mergeQVVars = (siteQVVarsFile, upgQVVarsFile, PROD_DIR) => {
 	//Pass an array of promises (created by our fileReadPromise function) to read multiple files
@@ -78,6 +102,53 @@ const QVVarsMergeProcess = (filesArray, PROD_DIR) => {
 		.then(fs.remove(path.join(PROD_DIR, 'Include/VariableEditor/data/SITE_qvVariables.JSON')))
 		.then(() => 'qvVariable and SITE_qvVariable files merged.')
 		.catch((err) => `Merge Files Error: ${err}`);
+};
+
+
+//==========================================
+//--Entry point for merging the QVVars files
+const mergeQVGroups = (siteQVVarsFile, upgQVVarsFile, PROD_DIR) => {
+	//Pass an array of promises (created by our fileReadPromise function) to read multiple files
+	//Once all promised have been resolved, call the processFiles function
+	//First file passed is the SiteFile JSON
+	//Second file passed is the upgradeFile JSON
+	return Promise.all([fileReadPromise(siteQVVarsFile), fileReadPromise(upgQVVarsFile)])
+	 	.then(data => QVGroupsMergeProcess(data, PROD_DIR));
+};
+//Function that will process the files once they have been read
+//This is the main function that does the compare and creates the new final qvGroups.json file
+const QVGroupsMergeProcess = (filesArray, PROD_DIR) => {
+	//Load and parse the siteFile and upgradeFile
+	let siteFile = JSON.parse(filesArray[0]);
+	let upgradeFile = JSON.parse(filesArray[1]);
+	//Convert the upgradeFile array to an object:
+	// {id: {name, description, ...}}
+	// id will be the uuid.  The idea being that we can more easily replace user modified variables
+	var upgradeObject = upgradeFile.reduce((newObj, currItem) => {
+	  newObj[currItem.id] = currItem;
+	  return newObj;
+	}, {});
+	//!! Array to hold the files we are going to write
+	let writeArray = [];
+	//Get all the objects that have been modified by a user
+	let userModified = siteFile.filter(varObj => varObj.modifyUser === 'user');
+
+	//let userModifiedToWrite = userModified; //because we are making changed directly to userModified later -- I know be immutable -- just lazy right now.
+	//!!Push a write promise to writeArray that will write out the userModified file for a reference
+	writeArray.push(fs.writeFile(path.join(PROD_DIR, 'Include/VariableEditor/data/qvVarsUserModified.JSON'), JSON.stringify(userModified)));
+
+	//Loop through the usermodified array and replace in the upgradeObject, the items that have been userModified
+  userModified.forEach(varObj => upgradeObject[varObj.id] = varObj);
+  //Convert upgradeObject back into an array:
+  let newUpgradeFile = Object.keys(upgradeObject).map(objKey => upgradeObject[objKey]);
+
+	//!!Push a write promise to writeArray that will write out the new qvVariables.json file
+	writeArray.push(fs.writeFile(path.join(PROD_DIR, 'Include/VariableEditor/data/qvVariables.JSON'), JSON.stringify(newUpgradeFile)));
+
+	return Promise.all(writeArray)
+		.then(fs.remove(path.join(PROD_DIR, 'Include/VariableEditor/data/SITE_qvVariables.JSON')))
+		.then(() => 'qvVariable and SITE_qvVariable files merged.')
+		.catch((err) => `Merge Files Error: ${err}`);
   // //write out the new file
   // fs.writeFile(path.join(PROD_DIR, 'Include/VariableEditor/data/qvVariables.JSON'), JSON.stringify(newUpgradeFile), err => {
   //   if (err) throw err;
@@ -88,5 +159,7 @@ const QVVarsMergeProcess = (filesArray, PROD_DIR) => {
 };
 
 module.exports = {
-	mergeQVVars
+	writeXMLData,
+	mergeQVVars,
+	mergeQVGroups
 }
